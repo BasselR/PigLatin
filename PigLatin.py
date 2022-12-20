0e18c7e5d7f700fc79303f4a7e8c69b03e450df2
Bassel <mail2bassel@yahoo.com>
2022-12-19 23:28:33 -0500

    Update PigLatin.py
    Improve commenting according to a more relevant standard

--- PigLatin.py
+++ PigLatin.py
@@ -1,20 +1,22 @@
 #Takes input string
 
 def piggifyWord(words):
-    
-    words = words.split()   #Splits input into list of individual words
-    pigLatin = ""   #Creates the string where we will store our output
-    
-    for i in words:     #For each word, create a new string 
-                                    #With first character at the end, + ay for pig latin
-        if i == 0:                  #If it's the first word of the list, capitalize it
+    # Splits input into list of individual words
+    words = words.split()
+    # Creates the string where we will store our output
+    pigLatin = "" 
+    # For each word, create a new string
+    for i in words: 
+        # With first character at the end, + ay for pig latin
+        # If it's the first word of the list, capitalize it
+        if i == 0:
             pigLatinWord = words[i][1:].capitalize() + words[i][0].lower() + "ay"
             
         else:
             pigLatinWord = words[i][1:] + words[i][0].lower() + "ay"
-            
-        pigLatin += (" " + pigLatinWord)    #Appends each new word to final output string
+        # Appends each new word to final output string
+        pigLatin += (" " + pigLatinWord)
         
-    print (pigLatin)    #Print output pig latin
+    print (pigLatin)
     
-piggifyWord("Look at that girl in the red sweater") #Call pig latin function
+piggifyWord("Look at that girl in the red sweater")
